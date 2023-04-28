ec3d1ab2440d196ce09b2894ef76c7617e8040c0
Yu Yongwoo <me@yowu.dev>
2023-04-28 18:06:10 +0900

    add seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Footer from './components/Footer';
+import { NextSeo } from 'next-seo';
 import dynamic from 'next/dynamic';
 const JsonPrettyViewer = dynamic(
   () => import('./components/JsonPrettyViewer'),
@@ -8,22 +8,41 @@ const JsonPrettyViewer = dynamic(
 );
 
 export default function Home() {
+  const seoConfig = {
+    title: 'Simple JSON Pretty Viewer Online by Yowu',
+    description:
+      'Simple JSON Pretty Viewer I made because the company blocked all kinds of Json Pretty Viewer Online, so I just asked ChatGPT to make it.',
+    openGraph: {
+      images: [
+        {
+          url: '/yowu-logo.jpeg', // public 폴더에 있는 이미지 파일 경로
+          width: 800,
+          height: 600,
+          alt: 'Simple JSON Pretty Viewer Online by Yowu',
+        },
+      ],
+    },
+  };
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">
-        Simple Json Pretty Viewer{' '}
-        <small>
-          by{' '}
-          <a
-            href="https://yowu.dev"
-            className="text-blue-600 hover:text-blue-800"
-          >
-            Yowu
-          </a>
-        </small>
-      </h1>
-      <JsonPrettyViewer />
-      {/* <Footer /> */}
-    </div>
+    <>
+      <NextSeo {...seoConfig} />
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">
+          Simple JSON Pretty Viewer Online{' '}
+          <small>
+            by{' '}
+            <a
+              href="https://yowu.dev"
+              className="text-blue-600 hover:text-blue-800"
+            >
+              Yowu
+            </a>
+          </small>
+        </h1>
+        <JsonPrettyViewer />
+        {/* <Footer /> */}
+      </div>
+    </>
   );
 }
